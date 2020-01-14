ab41b42e1d528ac64f75983bd84a418f502eb2ce
DICE777 <seulgi7383@gmail.com>
2020-01-14 23:48:47 +0900

    Making a frame : Get the image of url, Write the tag

--- app/src/main/java/com/example/bestofthebestproject/MainActivity.java
+++ app/src/main/java/com/example/bestofthebestproject/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.bestofthebestproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String url = "http://www.booking.com/Share-kJfXaQb";
+
+    private EditText mUrlEditText;
+    private Button mInputButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUrlEditText = findViewById(R.id.url_edit_text);
+        mUrlEditText.setText(url);
+
+        mInputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
